7a4ec3b34557363e348fcf8305044a2537952166
Jasan Hernandez <jasan_azael@hotmail.es>
2019-08-18 15:07:05 -0500

    production 0.0.3

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,10 @@ module.exports = (env) => {
           loader: 'babel-loader',
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
-            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-class-properties"
+            ]
           }
         },
         // usar para produccion.
